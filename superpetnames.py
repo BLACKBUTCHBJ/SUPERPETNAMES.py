cf0147e7402d1bb02c7c8bfa8b082d2e587691eb
Bettye <155570293+Bjbrono@users.noreply.github.com>
2024-07-19 09:30:05 -0500

    Create superpetnames.py

---
+++ superpetnames.py
@@ -0,0 +1,9 @@
+print("Welcome to the SuperPet Name Generator.")
+
+# Pets Input Information 
+PetsFirstName = input("Enter your pet's first name?: ")
+PetsPower = input("Enter your pet's cool power!: ")
+PetsNumber = input("Now give your pet a cool number!: ")
+
+# User Output 
+print("Okay here's your pets cool new name: ",PetsFirstName,PetsPower,PetsNumber)
